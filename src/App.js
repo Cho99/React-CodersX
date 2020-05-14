43295af9963ac02d8e0e91f853e0ddc11075eeb1
Glitch (componets-lesson6) <none>
2020-05-14 05:42:24 +0000

    🚞👆 Updated Form Login Coders-X

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Header.css';
+import './LoginForm.css';
+import Header from './components/Header';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
-        
-        
-       
+        <div className="container">
+           <Header/>
+           <LoginForm/>
+        </div> 
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,20 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-
-const todos = [
-  'Go to market',
-  'Buy food',
-  'Make dinner'
-];
-
-
-const Hello = <div className='title'> Hello </div>;
-let Items = todos.map(x => <li> {x} </li>);
-// console.log(JSON.parse(Items));
-const Todos = <ul>{Items}</ul>;
-console.log(Todos);
-ReactDOM.render(Todos, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
