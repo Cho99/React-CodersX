7155232b296fde7281d54dfaae1d743954fa6249
Glitch (bai2-props-lesson7-coders-x) <none>
2020-05-15 01:29:42 +0000

    🚀🗾 Bài 2-Props-Lesson 7

--- src/App.js
+++ src/App.js
@@ -8,7 +8,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-       <TabMenu/>
+       <TabMenu items={
+            [
+              {className: 'Home active' ,icon: 'https://cdn.glitch.com/8c389762-2f82-439a-82ac-d8ba588da565%2Fhome.png?v=1589464219359', label:'HOME'},
+              {className: 'Deals' ,icon: 'https://cdn.glitch.com/8c389762-2f82-439a-82ac-d8ba588da565%2Flove-and-romance.png?v=1589500891207', label:'DEALS'},
+              {className: 'Upload' ,icon: 'https://cdn.glitch.com/8c389762-2f82-439a-82ac-d8ba588da565%2Fmultimedia.png?v=1589464214079', label:'UPLOAD'},
+              {className: 'Work' ,icon: 'https://cdn.glitch.com/8c389762-2f82-439a-82ac-d8ba588da565%2Ffood-and-restaurant.png?v=1589464236041', label:'WORK'},
+              {className: 'Setting' ,icon: 'https://cdn.glitch.com/8c389762-2f82-439a-82ac-d8ba588da565%2Finterface.png?v=1589464285191', label:'SETTING'},
+              
+            ]
+        }/>
       </div>
     );
   }
--- src/components/TabMenu.js
+++ src/components/TabMenu.js
@@ -2,29 +2,18 @@ import React, { Component } from 'react';
 
 class TabMenu extends Component {
   render() {
+    // console.log(this.props.items);
+    const items = this.props.items.map(x =>                           
+       <div key={x.index} className={x.className}>
+            <img src={x.icon}></img>
+            <span>{x.label}</span>
+      </div> 
+    ); 
+                                       console.log(items);
     return (
       <div className="TabMenu">
         <div className="Menu">
-          <div className="Home active">
-            <img src="https://cdn.glitch.com/8c389762-2f82-439a-82ac-d8ba588da565%2Fhome.png?v=1589464219359"></img>
-            <span>HOME</span>
-          </div>
-          <div className="Deals">
-            <img src="https://cdn.glitch.com/8c389762-2f82-439a-82ac-d8ba588da565%2Flove-and-romance.png?v=1589500891207"></img>
-            <span>DEALS</span>
-          </div>
-          <div className="Upload">
-            <img src="https://cdn.glitch.com/8c389762-2f82-439a-82ac-d8ba588da565%2Fmultimedia.png?v=1589464214079"></img>
-            <span>UPLOAD</span>
-          </div>
-          <div className="Work">
-            <img src="https://cdn.glitch.com/8c389762-2f82-439a-82ac-d8ba588da565%2Ffood-and-restaurant.png?v=1589464236041"></img>
-            <span>WORK</span>
-          </div>
-          <div className="Setting">
-            <img src="https://cdn.glitch.com/8c389762-2f82-439a-82ac-d8ba588da565%2Finterface.png?v=1589464285191"></img>
-            <span>SETTINGS</span>
-          </div>
+          {items}
         </div>
       </div>
     );
