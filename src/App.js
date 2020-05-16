bd02d5c1b189222e2a5518f04559e7cf2848ae3d
Glitch (conditional-rendering-lesson11) <none>
2020-05-16 05:23:17 +0000

    🌱👓 conditional-rendering-lesson11

--- src/App.js
+++ src/App.js
@@ -1,44 +1,15 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import RecommnededFriends from "./components/RecommendedFriends";
-import "./RecommendedFriends.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Notification from'./components/Notification';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.friends = [
-      {
-        avatar:
-          "https://cdn.glitch.com/c6e3ccf2-1998-43a2-a093-ffec7b705b94%2Fimages.jpg?v=1589542450577",
-        name: "Dog99"
-      },
-      {
-        avatar:
-          "https://cdn.glitch.com/c6e3ccf2-1998-43a2-a093-ffec7b705b94%2Fimages.jpg?v=1589542450577",
-        name: "Chanh99"
-      },
-      {
-        avatar:
-          "https://cdn.glitch.com/c6e3ccf2-1998-43a2-a093-ffec7b705b94%2Fimages.jpg?v=1589542450577",
-        name: "Quat99"
-      }
-    ];
-  }
+  
   render() {
+    const hasUnread = {hasUnread: true};
     return (
       <div className="App">
-        <div className="container">
-          <div className="recommend-friends">
-            <div className="header">
-              <span className="recommend">Gợi ý cho bạn</span>
-              <a href="#" className="all">
-                Xem tất cả
-              </a>
-            </div>
-              <RecommnededFriends  friends={this.friends} />
-          </div>
-        </div>
+        <Notification hasUnread={hasUnread}/>
       </div>
     );
   }
